edbdb974772dd8989cd08cd987da07a9fac1c951
Dawson Friesenhahn <dfreeze11@tamu.edu>
2024-04-16 02:32:25 -0600

    coming along nicely, but still have a problem where cards grow as you add more. Need to fix getTkImageForCard()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from Cards.Deck import Deck
+from Cards.Hand import Hand
 from PIL import Image, ImageTk
 from time import sleep
 
@@ -9,28 +10,91 @@ class Blackjack:
         self.otherPlayers= otherPlayers
         self.numDecks= numDecks
         self.otherPlayerHands= [ [] for i in range(otherPlayers)]
-        self.playerHand= []
-        self.dealerHand= []
+        self.playerHand= Hand()
+        self.dealerHand= Hand()
 
         self.deck= Deck(numDecks)
+        
         self.root= root
+        
         self.player_frame= ttk.Frame(self.root, width=100, height=100, padding=10)
         self.player_frame.grid(row=1, column=0)
+        self.showPlayerTotal= True
+        self.playerTotalLabel= ttk.Button(root, text="Player Total: ", command= self.togglePlayerHandTotalLabel)
+        self.playerTotalLabel.grid(row=1, column=2)
+
         self.dealer_frame= ttk.Frame(self.root, width= 100, height= 100, padding= 10)
         self.dealer_frame.grid(row=0, column=0)
+        self.dealerDownCardTk= None
+        self.dealerDownCard= None
 
         self.hitButton= ttk.Button(root, text="Hit", command=self.hit)
         self.hitButton["state"] = "disabled"
         self.hitButton.grid(column=0, row=2)
-        self.standButton= ttk.Button(root, text="Stand", default="disabled")
+        self.standButton= ttk.Button(root, text="Stand", command=self.stand)
         self.standButton.grid(column=1, row=2)
         self.dealButton= ttk.Button(root, text= "Deal", command=self.deal)
         self.dealButton.grid(column=2, row= 2) 
 
-
         self.photo_images= []
 
+    def togglePlayerHandTotalLabel(self):
+        self.showPlayerTotal = not self.showPlayerTotal
+        self.updateHandTotalLabel(self.playerTotalLabel, self.playerHand)
+
+    def updateHandTotalLabel(self, label, hand):
+        if self.showPlayerTotal:
+            self.playerTotalLabel.config(text= f"Player Total: {hand.calculateTotal()}")
+        else:
+            self.playerTotalLabel.config(text= "Show player hand total")
+
     def addCardToTk(self, frame, card):
+        tkImage= self.getTkImageForCard(card, frame)
+        tkCard = ttk.Label(frame, image=tkImage)
+        tkCard.pack(side="left")
+        # Keep a reference to the PhotoImage object
+        self.photo_images.append(tkImage)
+        self.root.update()
+        return tkCard
+
+    def deal(self):
+        self.photo_images.clear()
+        self.dealerHand.clear()
+        self.playerHand.clear()
+        
+        
+        #deal two to main player first
+        for i in range(2):
+            newCard= self.deck.draw()
+            self.playerHand.append(newCard)
+            self.addCardToTk(self.player_frame, newCard)     
+            self.updateHandTotalLabel(self.playerTotalLabel, self.playerHand)    
+        
+
+
+        #deal two each extra player
+        
+        #deal one face down to dealer
+        self.dealerDownCard= self.deck.draw()
+        self.dealerDownCard.show_back= True
+        self.dealerHand.append(self.dealerDownCard)
+        self.dealerDownCardTk= self.addCardToTk(self.dealer_frame, self.dealerDownCard)
+
+        #deal one face up to dealer
+        newCard= self.deck.draw()
+        self.dealerHand.append(newCard)
+        self.addCardToTk(self.dealer_frame, newCard)
+
+        self.hitButton['state']= "enabled"
+        self.standButton['state'] = 'enabled'
+
+    def hit(self):
+        newCard= self.deck.draw()
+        self.playerHand.append(newCard)
+        self.addCardToTk(self.player_frame, newCard)
+        self.updateHandTotalLabel(self.playerTotalLabel, self.playerHand) 
+
+    def getTkImageForCard(self, card, frame):
         imgName= "./Images/" + card.getFullName() + ".png"
         print(imgName)
         img= Image.open(imgName)
@@ -51,46 +115,16 @@ class Blackjack:
         print(f"w: {new_width}, h:{new_height}")
         img = img.resize((new_width, new_height), Image.ANTIALIAS)
 
-        tkImage= ImageTk.PhotoImage(img)
-        tkCard = ttk.Label(frame, image=tkImage)
-        tkCard.pack(side="left")
-        # Keep a reference to the PhotoImage object
+        return ImageTk.PhotoImage(img)
+
+    def stand(self):
+        self.dealerDownCard.show_back= False
+        tkImage= self.getTkImageForCard(self.dealerDownCard, self.dealer_frame)
+        self.dealerDownCardTk.config(image=tkImage)
         self.photo_images.append(tkImage)
-        self.root.update()
-        sleep(0.5)
+        self.hitButton['state']= "disabled"
+        self.standButton['state']= 'disabled'
 
-    def deal(self):
-        self.photo_images.clear()
-
-        
-        
-        #deal two to main player first
-        for i in range(2):
-            newCard= self.deck.draw()
-            self.playerHand.append(newCard)
-            self.addCardToTk(self.player_frame, newCard)         
-        
-
-
-        #deal two each extra player
-        
-        #deal one face down to dealer
-        newCard= self.deck.draw()
-        newCard.show_back= True
-        self.dealerHand.append(newCard)
-        self.addCardToTk(self.dealer_frame, newCard)
-
-        #deal one face up to dealer
-        newCard= self.deck.draw()
-        self.dealerHand.append(newCard)
-        self.addCardToTk(self.dealer_frame, newCard)
-
-        self.hitButton['state']= "enabled"
-
-    def hit(self):
-        newCard= self.deck.draw()
-        self.playerHand.append(newCard)
-        self.addCardToTk(self.player_frame, newCard)
 
 
 def main():
